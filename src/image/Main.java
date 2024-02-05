b751a9f4b4ee2266a7db2aabb9574841b969c7de
Daryan <trinhdaryan@gmail.com>
2024-02-05 11:03:28 -0800

    Initial FIles

---
+++ src/image/Main.java
@@ -0,0 +1,14 @@
+package image;
+
+import images.APImage;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        TweakedAPImage image = new TweakedAPImage("smokey.jpg");
+        image.draw();
+    }
+
+
+}
